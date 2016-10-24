90b8de8a6058df4d8020d2b7875037efb0c13418
IlyaPyatkin <baldurmods@gmail.com>
2016-10-24 15:16:55 +0300

    Added password prompt

--- db.py
+++ db.py
@@ -2,18 +2,23 @@ import psycopg2
 import sys
 
 con = None
+print("Insert DB password:")
+password = input()
+
 
 try:
-    con = psycopg2.connect(database='hotel', user='postgres')
+    con = psycopg2.connect(database='hotel', user='postgres', password=password)
     cur = con.cursor()
 
     # Runs the given line as a SQL transaction
-    cur.execute(input())
+    print("Insert your SQL query:")
+    query = input()
+    cur.execute(query)
 
     con.commit()
     rows = cur.fetchall()
     for row in rows:
-        print(row, end="\n")
+        print(row)
 
 
 except psycopg2.DatabaseError as e:
