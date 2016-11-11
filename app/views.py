1788e855bc489bddb480520926b21f65a8d25ad1
IlyaPyatkin <baldurmods@gmail.com>
2016-11-11 20:11:57 +0300

    Added some example/test features

--- app/views.py
+++ app/views.py
@@ -1,10 +1,26 @@
 from flask import render_template
 from app import app
+from db import run_query
 
 
 @app.route('/')
 def index():
-    user = {'nickname': 'Sam'}
+    name = run_query('SELECT name FROM person '
+                     'ORDER BY name ASC LIMIT 1')
+    user = {'nickname': name[0][0]}
     return render_template("index.html",
                            title='Home',
                            user=user)
+
+
+@app.route('/table/<table>')
+def show_table(table):
+    rows = run_query("SELECT * FROM " + table)
+    columns = run_query("SELECT column_name " +
+                        "FROM information_schema.columns " +
+                        "WHERE table_name = '" + table + "'")
+
+    return render_template("table.html",
+                           rows=rows,
+                           columns=columns,
+                           table=table)
