c90a80d4a4f5958be136e8e204fa031f03ae368e
IlyaPyatkin <baldurmods@gmail.com>
2016-11-11 20:11:35 +0300

    Updated db connection handling

--- db.py
+++ db.py
@@ -1,31 +1,33 @@
 import psycopg2
 import sys
 
-con = None
-print("Insert DB password:")
-password = input()
 
+def run_query(query):
+    connection = None
+    # print("Insert DB password:")
+    # password = input()
+    password = "rudlab"
 
-try:
-    con = psycopg2.connect(database='hotel', user='postgres', password=password)
-    cur = con.cursor()
+    try:
+        connection = psycopg2.connect(database='hotel', user='postgres', password=password)
+        cursor = connection.cursor()
 
-    # Runs the given line as a SQL transaction
-    print("Insert your SQL query:")
-    query = input()
-    cur.execute(query)
+        # Runs the given line as a SQL transaction
+        cursor.execute(query)
+        connection.commit()
+        output = cursor.fetchall()
 
-    con.commit()
-    rows = cur.fetchall()
-    for row in rows:
+    except psycopg2.DatabaseError as e:
+        print('Error %s' % e)
+        sys.exit(1)
+
+    finally:
+        if connection:
+            connection.close()
+
+    return output
+
+if __name__ == '__main__':
+    print("Input your query:")
+    for row in run_query(input()):
         print(row)
-
-
-except psycopg2.DatabaseError as e:
-    print('Error %s' % e)
-    sys.exit(1)
-
-
-finally:
-    if con:
-        con.close()
